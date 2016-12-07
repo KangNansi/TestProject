9c8458afc1c5c7016f552c1336068e69d3556e4d
KangNansi <evillz@hotmail.com>
2016-12-08 00:07:28 +0100

    blablabla

--- TestProject/Assets/Scripts/TileMap.cs
+++ TestProject/Assets/Scripts/TileMap.cs
@@ -7,6 +7,10 @@ public class TileMap : MonoBehaviour {
     public int size_x;
     public int size_y;
     public Texture texture;
+    public int tile_width;
+    public int tile_height;
+    float tileSizeX=1.0f;
+    float tileSizeY=1.0f;
     public class Tile
     {
         int x, y;
@@ -25,6 +29,31 @@ public class TileMap : MonoBehaviour {
         
 	}
 
+    public void setTile(int x, int y, int tx, int ty)
+    {
+        Vector2[] uv = GetComponent<MeshFilter>().sharedMesh.uv;
+        int quadIndex = (x + (y * size_x)) * 4;
+        uv[quadIndex] = new Vector2(tx * tileSizeX, ty * tileSizeY);
+        uv[quadIndex+1] = new Vector2(tx * tileSizeX, (ty+1) * tileSizeY);
+        uv[quadIndex+2] = new Vector2((tx+1) * tileSizeX, (ty+1) * tileSizeY);
+        uv[quadIndex+3] = new Vector2((tx+1) * tileSizeX, ty * tileSizeY);
+        GetComponent<MeshFilter>().sharedMesh.uv = uv;
+    }
+
+    public void randomizeTile()
+    {
+        int maxTx = texture.width/tile_width;
+        int maxTy = texture.height / tile_height;
+        for (int i = 0; i < size_x * size_y; i++)
+            setTile(i % size_x, i / size_x, Random.Range(0, maxTx), Random.Range(0, maxTy));
+    }
+
+    public void fillTile(int tx, int ty)
+    {
+        for (int i = 0; i < size_x * size_y; i++)
+            setTile(i % size_x, i / size_x, tx, ty);
+    }
+
     public void CreateMesh()
     {
         if (size_x < 1) size_x = 1;
@@ -33,7 +62,11 @@ public class TileMap : MonoBehaviour {
         Vector3[] vertices = new Vector3[nb_tiles*4];
         int[] triangles = new int[nb_tiles*6];
         Vector2[] uv = new Vector2[nb_tiles*4];
-        
+        if (texture != null)
+        {
+            tileSizeX = tile_width / (float)texture.width;
+            tileSizeY = tile_height / (float)texture.height;
+        }
 
         for(int i=0; i<nb_tiles; i++)
         {
@@ -42,15 +75,15 @@ public class TileMap : MonoBehaviour {
             //Set Tile in order : Left-Bottom, Left-Up, Right-Up, Right-Bottom
             //Setting each tile vertices
             vertices[quadIndex] = new Vector3(i%size_x, i/size_x, 0);
-            vertices[quadIndex + 1] = new Vector3(i % size_x, i / size_x + 1, 0);
-            vertices[quadIndex + 2] = new Vector3(i % size_x + 1, i / size_x + 1, 0);
-            vertices[quadIndex + 3] = new Vector3(i % size_x + 1, i / size_x, 0);
+            vertices[quadIndex + 1] = new Vector3(i % size_x, i / size_x + 1.01f, 0);
+            vertices[quadIndex + 2] = new Vector3(i % size_x + 1.01f, i / size_x + 1.01f, 0);
+            vertices[quadIndex + 3] = new Vector3(i % size_x + 1.01f, i / size_x, 0);
 
             //Settings each tile uv coords
             uv[quadIndex] = new Vector2(0,0); 
-            uv[quadIndex + 1] = new Vector2(0, 1);
-            uv[quadIndex + 2] = new Vector2(1, 1);
-            uv[quadIndex + 3] = new Vector2(1, 0);
+            uv[quadIndex + 1] = new Vector2(0, tileSizeY);
+            uv[quadIndex + 2] = new Vector2(tileSizeX, tileSizeY);
+            uv[quadIndex + 3] = new Vector2(tileSizeX, 0);
 
             //Settings each tile triangles
             //First Triangle
--- TestProject/Assets/Scripts/TileMapEditor.cs
+++ TestProject/Assets/Scripts/TileMapEditor.cs
@@ -5,7 +5,8 @@ using UnityEditor;
 [CanEditMultipleObjects]
 public class TileMapEditor : Editor {
     public Vector2 scrollPosition;
-
+    int tx=0;
+    int ty=0;
     void OnEnable()
     {
 
@@ -23,21 +24,41 @@ public class TileMapEditor : Editor {
 
     public override void OnInspectorGUI()
     {
+
+        
+
         DrawDefaultInspector();
         var t = (target as TileMap);
+        Rect texRect = new Rect(0, 0, t.texture.width, t.texture.height);
         if (GUILayout.Button("Generate"))
         {
             (target as TileMap).CreateMesh();
         }
-        //GUILayout.BeginScrollView()
-        scrollPosition = GUI.BeginScrollView(new Rect(0, 0, 200, 200), scrollPosition, new Rect(0, 0, t.texture.width, t.texture.height));
+        if (GUILayout.Button("Randomize"))
+        {
+            (target as TileMap).randomizeTile();
+        }
+        if (GUILayout.Button("Fill"))
+        {
+            (target as TileMap).fillTile(tx, (t.texture.height/t.tile_height-1)-ty);
+        }
+
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUIStyle.none, GUILayout.Height(200));
+
+        //Events
+        if (Event.current.type == EventType.MouseDown && texRect.Contains(Event.current.mousePosition))
+        {
+            tx = (int)Event.current.mousePosition.x/t.tile_width;
+            ty = (int)Event.current.mousePosition.y/t.tile_height;
+            this.Repaint();
+        }
+
+        GUILayout.Label(t.texture, GUIStyle.none, GUILayout.Width(t.texture.width), GUILayout.Height(t.texture.height));
+        EditorGUI.DrawRect(new Rect(tx * t.tile_width, ty * t.tile_height, t.tile_width, t.tile_height)
+            , new Color(1, 1, 1, 0.2f));
         
-       // EditorGUILayout.RectField(new Rect(0, 0, t.texture.width, t.texture.height));
-        //GUILayout.;
-        GUI.DrawTexture(new Rect(0, 0, t.texture.width, t.texture.height), (target as TileMap).texture);
-        //GUILayout.EndArea();
-        GUI.EndScrollView();
-        EditorGUILayout.EndVertical();
+        //EditorGUI.DrawPreviewTexture(new Rect(0, 0, t.texture.width, t.texture.height), t.texture);
+        EditorGUILayout.EndScrollView();
     }
 
     public void OnSceneGUI()
